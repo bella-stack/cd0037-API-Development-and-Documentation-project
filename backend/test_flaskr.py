07dc17196f5c9cac851472a2abe357b95fd7b694
bella <hwang895@gatech.edu>
2022-02-13 22:09:34 -0600

    add add_question, delete_question unit tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path ="postgres://{}:{}@{}/{}".format('student', 'student','localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -29,12 +29,17 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+    def testAddNewQuestion(self):
+        res = self.client().post('/questions', json=self.new_question)
+        self.assertEqual(res.status_code, 200)
+        self.assertMultiLineEqual(json.loads(res.data), self.new_question['question'])
 
-
+    def testDeleteQuestion(self):
+        res = self.client().delete('/questions/' + str(Question.query.first().id))
+        question = Question.query.filter(Question.id == 1).first()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(question, None)
+        
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
